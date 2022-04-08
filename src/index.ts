cc188482c65237c1b2d4f5f7182455cc3016f105
ihm-tswow <ihm-ts@protonmail.com>
2022-04-08 18:49:46 +0200

    split up message handler
    - split by thread/non-thread messages

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client, Intents, Message, TextChannel } from 'discord.js'
+import { Client, Intents, Message, TextChannel, ThreadChannel } from 'discord.js'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -50,43 +50,52 @@ async function getTopic (message: Message<boolean>) {
 
 const channels = (process.env.CHANNELS || '').split(',')
 
+async function handleThreadMessage (message: Message<boolean>, channel: ThreadChannel) {
+  const startMessage = await channel.fetchStarterMessage()
+  if (!channels.includes(startMessage.channelId)) {
+    return
+  }
+
+  if (!message.content.startsWith(RENAME_COMMAND)) {
+    return
+  }
+
+  const rename = message.content.substring(RENAME_COMMAND.length)
+  if (rename.length > 0 && startMessage.author.id === message.author.id) {
+    channel.setName(rename)
+  }
+}
+
+async function handleChannelMessage (message: Message<boolean>, channel: TextChannel) {
+  if (!channels.includes(message.channelId)) {
+    return
+  }
+
+  if (!isGalleryMessage(message)) {
+    await message.author.send(getDeleteText(message, channel))
+    await message.delete()
+  } else {
+    const thread = await channel.threads
+      .create({ startMessage: message.id, name: await getTopic(message) })
+    thread.members.add(message.author)
+  }
+}
+
 client.on('ready', () => {
   console.log('Gallery Bot ready...')
 })
 
 client.on('messageCreate', async message => {
   try {
-    if (message.channel.type === 'GUILD_PUBLIC_THREAD') {
-      const startMessage = await message.channel.fetchStarterMessage()
-      if (!channels.includes(startMessage.channelId)) {
-        return
+    switch (message.channel.type) {
+      case 'GUILD_PUBLIC_THREAD': {
+        await handleThreadMessage(message, message.channel)
+        break
       }
-
-      if (!message.content.startsWith(RENAME_COMMAND)) {
-        return
+      case 'GUILD_TEXT': {
+        await handleChannelMessage(message, message.channel)
+        break
       }
-
-      const rename = message.content.substring(RENAME_COMMAND.length)
-      if (rename.length > 0 && startMessage.author.id === message.author.id) {
-        message.channel.setName(rename)
-      }
-    }
-
-    if (!channels.includes(message.channelId)) {
-      return
-    }
-
-    if (message.channel.type !== 'GUILD_TEXT') {
-      return
-    }
-
-    if (!isGalleryMessage(message)) {
-      await message.author.send(getDeleteText(message, message.channel))
-      await message.delete()
-    } else {
-      const thread = await message.channel.threads
-        .create({ startMessage: message.id, name: await getTopic(message) })
-      thread.members.add(message.author)
     }
   } catch (err) {
     console.log(err)
